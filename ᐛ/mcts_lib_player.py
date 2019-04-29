f7b1fa432ce8c9983d48518773bdb99f8b675284
Eana Hufwe <ilove@1a23.com>
2019-04-29 22:17:47 +1000

    Add library based MCTS player for performance metrics.

---
+++ ᐛ/mcts_lib_player.py
@@ -0,0 +1,47 @@
+from random import choice
+from mcts import mcts
+
+from .board import Board
+
+NEXT_PLAYER = {
+    "red": "green",
+    "green": "blue",
+    "blue": "red"
+}
+
+
+class MCTSState(Board):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.color = "red"
+
+
+    def getPossibleActions(self):
+        return [(i, self.color) for i in self.possible_actions(self.color)]
+    
+    def takeAction(self, action):
+        ac, co = action
+        b = self.move(co, ac)
+        b.color = NEXT_PLAYER[co]
+        return b
+
+    def isTerminal(self):
+        return self.get_exited_pieces("red") >= 4 or \
+                self.get_exited_pieces("green") >= 4 or \
+                self.get_exited_pieces("blue") >= 4
+    
+    def getReward(self):
+        return 1 if self.get_exited_pieces(self.color) >= 4 else -1
+
+
+class Player:
+    def __init__(self, color):
+        self.color = color
+        self.board = MCTSState()
+        self.mcts = mcts(timeLimit=1000)
+
+    def action(self):
+        return self.mcts.search(initialState=self.board)[0]
+
+    def update(self, colour, action):
+        self.board = self.board.takeAction((action, colour))
